ef69b43e01a64f577374ed6dd596bd613b77548f
DESKTOP-F7ILDG9\撞见、 <312043814@qq.com>
2019-04-15 21:17:08 +0800

    api调用

--- utils/config/api.js
+++ utils/config/api.js
@@ -5,26 +5,16 @@ import {
 
 //所有的请求
 const ALL_API = {
+  //登陆授权
   getOpenId: {
     method: 'POST',
     url: 'getOpenId'
-  },
-  generatePayQrCode: {
-    method: 'POST',
-    url: 'generateQrCode'
-  },
-  getTimes: {
-    method: 'POST',
-    url: 'getTimes'
   },
   submitForm: {
     method: 'POST',
     url: 'submitForm'
   },
-  updateVersion: {
-    method: 'POST',
-    url: 'updateVersion'
-  },
+
 }
 
 const Api = new MyHttp({}, ALL_API);
